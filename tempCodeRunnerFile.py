b8ccff37e8e6c9613e305297052cabc9a1caff5f
jass2k5 <jsssndl@gmail.com>
2025-08-16 20:18:38 +0530

    Added a README File

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,25 @@
+from src.notes import Notes
+def main():
+    print("welcome to notebook")
+    print("please enter your taskname")
+    filename = "data/notes.json"
+    access = Notes(filename)
+    while True:
+        try:
+            ask = int(input("1: add, 2: show data, 3: delete a note, 4: delete all data, 5: edit note"))
+        except (ValueError or TypeError):
+            print("enter valid number")
+            continue
+        if ask == 1:
+            access.add_note()
+        elif ask == 2:
+            access.show_stored()
+        elif ask == 3:
+            access.delete_note()
+        elif ask == 4:
+            access.delete_all_data()
+        elif ask == 5:
+            access.edit_the_note()
+    
+if __name__ == "__main__":
+    main()
